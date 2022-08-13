eca048d59bfd537ff937d27c55df2311ffdc9761
Yi Jia Huang <yijh1705@gmail.com>
2022-08-13 14:12:52 -0400

    Initial Commit

---
+++ magnetic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  magnetic
+//
+//  Created by HDSB on 2022-08-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ magnetic/magneticApp.swift
@@ -0,0 +1,17 @@
+//
+//  magneticApp.swift
+//  magnetic
+//
+//  Created by HDSB on 2022-08-13.
+//
+
+import SwiftUI
+
+@main
+struct magneticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
