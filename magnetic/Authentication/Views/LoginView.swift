13014c53d1eb7c14e6f06311fc381bbb87c336e5
Yi Jia Huang <yijh1705@gmail.com>
2022-08-13 20:41:09 -0400

    created LoginView and RegistrationView

---
+++ magnetic/Authentication/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  magnetic
+//
+//  Created by HDSB on 2022-08-13.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ magnetic/Service/UserService.swift
@@ -0,0 +1,15 @@
+//
+//  UserService.swift
+//  magnetic
+//
+//  Created by HDSB on 2022-08-13.
+//
+
+import Foundation
+import Firebase
+
+struct UserService {
+    func fetchUser(withUid uid: String) {
+        print("IM GOING TO DO THIS LATER")
+    }
+}
