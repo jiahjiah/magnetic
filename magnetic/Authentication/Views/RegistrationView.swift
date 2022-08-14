8715305cfbbde4da6217fd15b4dea087bf54e749
Yi Jia Huang <yijh1705@gmail.com>
2022-08-13 21:35:50 -0400

    added registration page

--- magnetic/Authentication/Views/RegistrationView.swift
+++ magnetic/Authentication/Views/RegistrationView.swift
@@ -8,8 +8,66 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var fullname = ""
+    @Environment(\.presentationMode) var presentationMode
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        //header
+        VStack {
+            VStack(alignment: .leading) {
+                HStack { Spacer() }
+                Text("Get Started.")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                Text("Create your account")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+            }
+            .frame(height: 260)
+            .padding(.leading)
+            .background(.teal)
+            .foregroundColor(.white)
+            .clipShape(RoundedShape(corners: [.bottomRight]))
+            
+            //fields
+            VStack(spacing: 40) {
+                TextField("Email", text: $email)
+                TextField("Full Name", text: $password)
+                TextField("Password", text: $password)
+            }
+            .padding(.horizontal, 32)
+            .padding(.top, 44)
+            
+            //button
+            Button {
+                print("Sign up here...")
+            } label: {
+                Image("Sign Up")
+            }
+            .padding(32)
+
+            Spacer()
+            
+            Button {
+                presentationMode.wrappedValue.dismiss()
+            } label: {
+                HStack {
+                    Text("Already have an account?")
+                        .font(.footnote)
+                    
+                    Text("Sign In")
+                        .font(.body)
+                        .fontWeight(.semibold)
+                }
+            }
+            .padding(.bottom, 32)
+            .foregroundColor(Color.teal)
+            
+        }
+        .ignoresSafeArea()
+        
     }
 }
 
